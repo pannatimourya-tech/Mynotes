160de850f30f03d96436449ccad26b56a3c7f036
Mourya Pannati <pannatimourya@gmail.com>
2025-08-31 18:33:23 +0530

    Create Note.js
    google.com

---
+++ .github/workflows/src/src/src/src/components/src/components/src/pages/src/pages/src/pages/src/pages/backend/backend/backend/backend/routes/backend/models/backend/routes/backend/models/Note.js
@@ -0,0 +1,15 @@
+// backend/models/Note.js
+import mongoose from "mongoose";
+
+const noteSchema = new mongoose.Schema(
+  {
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    title: { type: String, required: true },
+    content: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+const Note = mongoose.model("Note", noteSchema);
+
+export default Note;
